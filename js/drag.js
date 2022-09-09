fdfdf3951611649e27abf2afd41c3a5a9907d90b
Fábio Gouveia <gouveia.fabio98@gmail.com>
2022-09-09 19:18:18 +0100

    update

--- js/drag.js
+++ js/drag.js
@@ -10,6 +10,18 @@ interact('.drag')
         ]
     })
 
+interact('.drag-parent')
+    .draggable({
+        listeners: { move: window.dragMoveListenerParent },
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'body',
+                endOnly: true
+            })
+        ]
+    })
+
 interact('.drag-resize')
     .resizable({
         // resize from all edges and corners
@@ -60,8 +72,64 @@ interact('.drag-resize')
         ]
     })
 
+interact('.resize-parent')
+    .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
+
+        listeners: {
+            move(event) {
+                var target = event.target.parentElement
+                var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+                // update the element's style
+                target.style.width = event.rect.width + 'px'
+                target.style.height = event.rect.height + 62 + 'px'
+                event.target.style.width = event.rect.width + 'px'
+                event.target.style.height = event.rect.height + 'px'
+
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left
+                y += event.deltaRect.top
+
+                target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
+            }
+        },
+        modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+                outer: 'body'
+            }),
+
+            // minimum size
+            interact.modifiers.restrictSize({
+                min: { width: 100, height: 50 }
+            })
+        ],
+
+        inertia: true
+    })
+
 function dragMoveListener(event) {
     var target = event.target
+        // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+    // translate the element
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+}
+
+function dragMoveListenerParent(event) {
+    var target = event.target.parentElement
         // keep the dragged position in the data-x/data-y attributes
     var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
     var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
